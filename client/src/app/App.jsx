342099dad2916eb252cd17853be8f2de3d401ac0
Nikita <norenkonikita@gmail.com>
2022-09-06 22:54:32 +0300

    eslint

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,22 +1,17 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    useLocation,
-} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { ToastContainer } from 'react-toastify';
+import { useDispatch } from 'react-redux';
 import Classes from '../components/classes/Classes';
 import Header from '../components/header/Header';
 import MainScreen from '../components/mainScreen/MainScreen';
 import Schedule from '../components/schedule/Schedule';
 import Trainers from '../components/trainers/Trainers';
 import RegisterPage from '../components/register/RegisterPage';
-import { AnimatePresence } from 'framer-motion';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
 import { LoginPage } from '../components/login/LoginPage';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import { getMe } from '../features/auth/authSlice';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const App = () => {
     const dispatch = useDispatch();
@@ -40,7 +35,7 @@ const App = () => {
                 <ToastContainer
                     position='bottom-center'
                     autoClose={2000}
-                    hideProgressBar={true}
+                    hideProgressBar
                     // icon={false}
                     // closeButton={false}
                 />
