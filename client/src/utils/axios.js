dfe57e08ba42368b15afc632aca7be07f5080905
Nikita <norenkonikita@gmail.com>
2023-02-21 13:09:50 +0200

    update url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://91.219.62.242:5100/api/',
+    baseURL: 'http://localhost:5100/api/',
     // baseURL: 'http://91.219.62.242:5100/api/',
     // http://91.219.62.242:5100/api/classes/get
 
